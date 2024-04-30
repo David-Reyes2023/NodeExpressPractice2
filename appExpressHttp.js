38f4f3dacc7e66e5002415ceaa5e3bacc17e5f45
David Reyes <david.reyes@tecnologicoliceocristiano.edu.ec>
2024-04-29 20:51:07 -0500

    ejercicio en express

--- appExpressHttp.js
+++ appExpressHttp.js
@@ -5,25 +5,34 @@ const app = express();
 app.use((req, res, next)=>{
     let body = '';
 
-    req.on ('end', ()=>{
-        const userName = body.split('=')[1]
+    req.on('end', ()=>{
+        const userName = body.split('=')[1];
         if(userName){
             console.log(userName);
             req.body = {name: userName};
         }
-        next();
-})
+        next(); 
+    });
 
-req.on('data', (chunk)=>{
-    body += chunk;
-});
-
+    req.on('data', (chunk)=>{
+        body += chunk;
+    });
+        
 });
 
 app.use((req,res,next)=>{
     if(req.body) {
         res.send('User: '+req.body.name);
     }
+    else{
+        res.send(`
+        <form method="POST">
+        <input type="text" name="user-name"/>
+        <button>Create User</button>
+        </form>
+        `);
+    }
+
 })
 
 app.listen(5001);
