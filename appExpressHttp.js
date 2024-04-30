897cef756f384958ac3cdf4f5e036a5ecba66d15
David Reyes <david.reyes@tecnologicoliceocristiano.edu.ec>
2024-04-29 20:39:00 -0500

    Primer cambio

---
+++ appExpressHttp.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next)=>{
+    let body = '';
+
+    req.on ('end', ()=>{
+        const userName = body.split('=')[1]
+        if(userName){
+            console.log(userName);
+            req.body = {name: userName};
+        }
+        next();
+})
+
+req.on('data', (chunk)=>{
+    body += chunk;
+});
+
+});
+
+app.use((req,res,next)=>{
+    if(req.body) {
+        res.send('User: '+req.body.name);
+    }
+})
+
+app.listen(5001);
